e25f32c0243d4342674847013bdc9e3961d13b19
HP <HP@stranger>
2023-12-22 12:45:56 +0530

    Initial commit

--- src/test/java/com/facegenie/TC/VerifyLoginFun.java
+++ src/test/java/com/facegenie/TC/VerifyLoginFun.java
@@ -18,22 +18,23 @@ public class VerifyLoginFun extends TestBaseClass {
 		
 	}
 	
-	@Test(priority=1)
 	
-	public void logoutFun() throws InterruptedException {
-		LoginPage aa= new LoginPage(driver);
-		aa.interEmailID();
-		log.info("inter email id");
-		aa.interPassword();
-		log.info("inter password");
-		aa.loginButton();
-		log.info("clicked on login button");
-		Thread.sleep(30000);
-		
-		HomePage home = new HomePage(driver);
-		home.logOut();
-		log.info("clicked on logout button");
-	}
+//	@Test(priority=1)
+//	
+//	public void logoutFun() throws InterruptedException {
+//		LoginPage aa= new LoginPage(driver);
+//		aa.interEmailID();
+//		log.info("inter email id");
+//		aa.interPassword();
+//		log.info("inter password");
+//		aa.loginButton();
+//		log.info("clicked on login button");
+//		Thread.sleep(30000);
+//		
+//		HomePage home = new HomePage(driver);
+//		home.logOut();
+//		log.info("clicked on logout button");
+//	}
 	
 
 }
